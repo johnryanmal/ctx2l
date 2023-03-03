39e0eddaf319a9c2ca4b5f83c29ed769b635dd65
johnryanmal <johnryanmal@gmail.com>
2023-03-03 11:12:32 -0800

    prototype file writer

--- python/main.py
+++ python/main.py
@@ -7,8 +7,17 @@ from ctx2lVisitor import ctx2lVisitor
 from ctx2lEvaluator import ctx2lEvaluator, ctx2lPythonEvaluator
 
 
-def main(path):
-    input_stream = FileStream(path)
+class Writer:
+    def __init__(self, path):
+        self.path = path
+
+    def write(self, filename, content):
+        with open(Path(self.path, filename), 'w') as file:
+            file.write(content)
+
+
+def main(input_path, output_path):
+    input_stream = FileStream(input_path)
     lexer = ctx2lLexer(input_stream)
     stream = CommonTokenStream(lexer)
     parser = ctx2lParser(stream)
@@ -16,24 +25,16 @@ def main(path):
     visitor = ctx2lVisitor()
     ast = visitor.visit(tree)
 
-    name = Path(path).stem
+    name = Path(input_path).stem
     evaluator = ctx2lEvaluator(name)
     generator = ctx2lPythonEvaluator(name)
     lexerFile, parserFile = evaluator.eval(ast)
     visitorFile = generator.eval(ast)
-    
-    print(f'=== {name}Lexer.g4 ===')
-    print(lexerFile)
 
-    print()
-
-    print(f'=== {name}Parser.g4 ===')
-    print(parserFile)
-
-    print()
-
-    print(f'=== {name}Visitor.py ===')
-    print(visitorFile)
+    writer = Writer(output_path)
+    writer.write(f'{name}Lexer.g4', lexerFile)
+    writer.write(f'{name}Parser.g4', parserFile)
+    writer.write(f'{name}Visitor.py', visitorFile)
 
 
 if __name__ == '__main__':
